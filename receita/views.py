5a7d17f569577186647b01383faa2c58d3d04146
Valdir Júnior <valdirmsjunior@gmail.com>
2026-03-11 16:20:53 -0300

    feat: Adicionar barra de pesquisa

--- receita/views.py
+++ receita/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Receita
+from django.db.models import Q # Importamos o Q para buscas complexas
 
-# Create your views here.
 def index(request):
     receitas = Receita.objects.filter(publicado=True)
-    return render(request, 'index.html', {'receitas': receitas})
+    search_term = request.GET.get('q')
+
+    if search_term:
+        # Filtra por título OU ingredientes que contenham o termo (icontains = case-insensitive)
+        receitas = receitas.filter(
+            Q(titulo__icontains=search_term) | 
+            Q(ingredientes__icontains=search_term)
+        )
+
+    return render(request, 'index.html', {
+        'receitas': receitas,
+        'search': search_term
+    })
 
 def detalhe_receita(request, slug):
     receita = get_object_or_404(Receita, slug=slug)
